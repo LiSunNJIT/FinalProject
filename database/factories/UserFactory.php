4a1f9761772c4c0f55b0364c3925afc897870889
li <ls447@njit.edu>
2019-05-15 23:09:46 -0400

    Added List of questions on the homepage.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
 
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$RG1draoWHODhHSX.MWKon..AewlISedb3S62KLGbBrR1JMQy8p1em', // password
         'remember_token' => Str::random(10),
     ];
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>My HomePage</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,8 +81,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome Here
                 </div>
+                <h1>This is My Final Project.</h1>
+                <h1>This is My Home Page.</h1>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
